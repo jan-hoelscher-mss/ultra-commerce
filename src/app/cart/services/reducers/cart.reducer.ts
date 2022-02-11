ea368379243fb0c52b07a7e10cbd1fe62212a38a
jan.hoelscher@codergrid.de <u7FGSHYjwZ5O>
2022-02-11 19:00:56 +0100

    - update reducer for immutable data

--- src/app/cart/services/reducers/cart.reducer.ts
+++ src/app/cart/services/reducers/cart.reducer.ts
@@ -6,30 +6,35 @@ export const initialState = [];
 export function cartReducer(state: any, action: any) {
   return createReducer(initialState,
     on(add, (state: any, {product}) => {
-      console.log("Add product:", product)
-      let products = Object.assign([], state);
-      const existingProduct = products.find((p: any) => {
-        return p.product.id == product.id;
+      let updated = false;
+      let data = state.map((p: any) => {
+        if(p.product.id != product.id){
+          return p;
+        }else{
+          updated = true;
+          return {product: product, quantity:p.quantity +1}
+        }
       });
-      if( existingProduct != null ){
-        existingProduct.quantity ++;
-      }else{
+      if(!updated){
         return state.concat([{product: product, quantity:1}])
       }
-      return products;
+      return data;
     }),
     on(update, (state: any, {product, quantity}) => {
       if(quantity > 0){
-        state.find((p: any) => {
-          return p.product.id == product.id;
-        }).quantity = quantity;
+        return state.map((p: any) => {
+          if(p.product.id != product.id){
+            return p;
+          }else{
+            return {product: product, quantity: quantity}
+          }
+        });
       }else{
         return state.filter((p: any) => {
           return p.product.id != product.id;
         });
       }
-      return state;
-    } ),
+    }),
     on(remove, (state: any, {product}) => {
       return state.filter((p: any) => {
         return p.product.id != product.id;
